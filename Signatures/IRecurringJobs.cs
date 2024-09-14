96df5a7b23e94e32dd81c7e8f4f1f45ebb721d47
dogruyolomerfurkan <dogruyolomerfurkan@gmail.com>
2024-09-14 13:32:13 +0300

    Recurring job signature added.

---
+++ Signatures/IRecurringJobs.cs
@@ -0,0 +1,11 @@
+﻿using Hangfire;
+
+namespace Signatures;
+
+public interface IRecurringJobs
+{
+    [DisableConcurrentExecution(60 * 2)]
+    [AutomaticRetry(Attempts = 3, LogEvents = true, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+    [Queue("Concurrent")]
+    void TestConcurrentExecution();
+}
